8fcf1c1c7fffd329c4a7bf98105dbe009eb6942f
João Francisco Bratti <jfranciscobratti@gmail.com>
2023-11-14 09:39:40 -0300

    atualizado

--- web/src/firebase-config.js
+++ web/src/firebase-config.js
@@ -1,3 +1,20 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyDX0wbbXACkh8yvaGEXuRacTaN-CHsGUNg",
+   authDomain: "friendlychat-1409c.firebaseapp.com",
+   projectId: "friendlychat-1409c",
+   storageBucket: "friendlychat-1409c.appspot.com",
+   messagingSenderId: "156326891517",
+   appId: "1:156326891517:web:35377cfe45a0a9a1541394"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
 /**
  * To find your Firebase config object:
  * 
